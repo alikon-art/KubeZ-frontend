db8607f42d5a8f486a8ea4e8238cc766d3247536
alikon-art <alikon-art@users.noreply.github.com>
2024-02-21 22:10:59 +0800

    现在反代地址是80了

--- src/utils/axios/axios.vue
+++ src/utils/axios/axios.vue
@@ -7,7 +7,7 @@
 
   // 基本配置
   const axiosInstance = axios.create({
-      baseURL: 'http://127.0.0.1:8080/api/',
+      baseURL: 'http://127.0.0.1:80/api/',
       headers: {
           'Content-Type': 'application/json'
       }
